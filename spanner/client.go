0404f35b12f2dc9719d328ded3415195d7275627
yoshidan <naohiro.y@gmail.com>
2021-02-01 11:28:01 +0900

    add spanner method

--- spanner/client.go
+++ spanner/client.go
@@ -19,6 +19,11 @@ type Reader interface {
 type Client interface {
 	Reader
 	ApplyOrBufferWrite(context.Context, ...*spanner.Mutation) error
+	Update(ctx context.Context, statement spanner.Statement) (int64, error)
+	UpdateWithOption(ctx context.Context, statement spanner.Statement, options spanner.QueryOptions) (int64, error)
+	BatchUpdate(ctx context.Context, statement []spanner.Statement) ([]int64, error)
+	PartitionedUpdate(ctx context.Context, statement spanner.Statement) (int64, error)
+	PartitionedUpdateWithOptions(ctx context.Context, statement spanner.Statement, options spanner.QueryOptions) (int64, error)
 }
 
 type DefaultTxClient struct {
@@ -44,6 +49,41 @@ func (e *DefaultTxClient) ApplyOrBufferWrite(ctx context.Context, data ...*spann
 	}
 	_, err := e.spannerClient.Apply(ctx, data)
 	return err
+}
+
+func (e *DefaultTxClient) Update(ctx context.Context, stmt spanner.Statement) (int64, error) {
+	if e.isInReadWriteTransaction() {
+		return e.txRW.Update(ctx, stmt)
+	}
+	return -1, errors.New("read write transaction is required to use statement")
+}
+
+func (e *DefaultTxClient) UpdateWithOption(ctx context.Context, stmt spanner.Statement, opts spanner.QueryOptions) (int64, error) {
+	if e.isInReadWriteTransaction() {
+		return e.txRW.UpdateWithOptions(ctx, stmt, opts)
+	}
+	return -1, errors.New("read write transaction is required to use statement")
+}
+
+func (e *DefaultTxClient) BatchUpdate(ctx context.Context, stmts []spanner.Statement) ([]int64, error) {
+	if e.isInReadWriteTransaction() {
+		return e.txRW.BatchUpdate(ctx, stmts)
+	}
+	return nil, errors.New("read write transaction is required to use statement")
+}
+
+func (e *DefaultTxClient) PartitionedUpdate(ctx context.Context, stmt spanner.Statement) (int64, error) {
+	if e.isInReadWriteTransaction() {
+		return -1, errors.New("partitioned update is unsupported in read write transaction")
+	}
+	return e.spannerClient.PartitionedUpdate(ctx, stmt)
+}
+
+func (e *DefaultTxClient) PartitionedUpdateWithOptions(ctx context.Context, stmt spanner.Statement, options spanner.QueryOptions) (int64, error) {
+	if e.isInReadWriteTransaction() {
+		return -1, errors.New("partitioned update is unsupported in read write transaction")
+	}
+	return e.spannerClient.PartitionedUpdateWithOptions(ctx, stmt, options)
 }
 
 func (e *DefaultTxClient) Read(ctx context.Context, table string, keys spanner.KeySet, columns []string) *spanner.RowIterator {
