9d612d8688dfbef328d7883c3c32813c00aa04f9
yoshidan <naohiro.y@gmail.com>
2021-02-06 19:41:47 +0900

    add hash slot func

---
+++ hashslot.go
@@ -0,0 +1,30 @@
+package gotx
+
+import (
+	"fmt"
+	"hash/crc32"
+)
+
+func GetHashSlotRange(size int, maxSlot uint32) []uint32 {
+	average := maxSlot / uint32(size)
+	maxValuePerShard := make([]uint32, size)
+	lastIndex := size - 1
+	for i := range maxValuePerShard {
+		if i == lastIndex {
+			maxValuePerShard[i] = maxSlot
+		} else {
+			maxValuePerShard[i] = average * uint32(i+1)
+		}
+	}
+	return maxValuePerShard
+}
+
+func GetIndexByHash(hashSlotRange []uint32, shardKey []byte, maxSlot uint32) (int, error) {
+	slot := crc32.ChecksumIEEE(shardKey) % maxSlot
+	for i, v := range hashSlotRange {
+		if slot < v {
+			return i, nil
+		}
+	}
+	return -1, fmt.Errorf("invalid key slot=%d", slot)
+}
